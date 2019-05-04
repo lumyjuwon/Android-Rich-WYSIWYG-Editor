87354430aaf56e5ebde78e27dd344a93ff342e0e
Seong JuWon <32597561+lumyjuwon@users.noreply.github.com>
2019-05-04 18:39:20 +0900

    Remove app and Add Sample

---
+++ sample/src/main/java/com/example/sample/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.sample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.lumyjuwon.richwysiwygeditor.RichWysiwyg;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RichWysiwyg wysiwyg;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
